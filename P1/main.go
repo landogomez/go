b9b853dc364b9038116a534cbe4a507322105ae3
jorge <ogomez.isc@gmail.com>
2024-12-09 13:48:29 -0600

    Server up

--- P1/main.go
+++ P1/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
@@ -19,4 +21,17 @@ func main() {
 	}
 
 	fmt.Println("PORT:", portString)
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server listening on port %s", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
